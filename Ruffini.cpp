50eb0ebc8052ffb65390cce9db09f731fd3a0b1d
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-03-24 16:08:22 +0100

    TestPush

--- Ruffini.cpp
+++ Ruffini.cpp
@@ -80,20 +80,3 @@ int main(){
     //std::cout << "x^3+3x^2+4x+2" << std::endl;
 	std::cin.ignore();
 }
-/*
-* DESCRIPTION
-On Friday, after the teachers' evening meeting, the math teacher went crazy.
-After drinking a few beers; talking to the chemistry and religion professors,
-he returned home and surrounded herself with ancient math books,
-candles and graphs drawn in chalk on the floor. . .
-the professor afterwards was never the same.
-Rumor has it that Monday morning, when he returned to school,
-his students were roasted by strange expressions followed by devastating grades and notes of demerit.
-Sure the school will go into action,
-but tomorrow morning at the first hour you know that you will have to face the teacher,
-and perhaps with a good gab and a clear mind you could be able to bring the teacher back to his hindsight.
-Or at least take a sufficient vote, and thus enter the legend.
-
-Constraints and assumptions
-
-*/
